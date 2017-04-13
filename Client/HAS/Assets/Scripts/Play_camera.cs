10db76db298ca65b21e94a3bc1afb7e313d2b53c
mykim31 <dnwjd146@naver.com>
2017-04-13 17:13:28 +0900

    유니티ㅇㅇ

---
+++ Client/HAS/Assets/Scripts/Play_camera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_camera : MonoBehaviour {
+    
+    Vector3 V3;
+    float X;
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+        
+        speed = 2.0f;
+    }
+
+    void Update()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+        
+        turn();
+    }
+
+    void turn()
+    {
+        X = Input.GetAxis("Mouse Y");
+        print("1"+X);
+        if (transform.rotation.eulerAngles.x - X <= 40f || transform.rotation.eulerAngles.x - X >= 320f)
+        {
+            V3 = new Vector3(-X, 0, 0);
+            transform.Rotate(V3 * speed);
+            print("2" + (transform.rotation.eulerAngles.x - X));
+        }
+    }
+}
---
+++ Client/HAS/Assets/Scripts/Player3.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3 : MonoBehaviour {
+
+    public GameObject player;
+    Camera player_camera;
+    Animator ani;
+    Vector3 addPosition;
+
+    float speed = 4.0f;
+    //float m_rotation_y = 0f;
+
+    // Use this for initialization
+    void Awake() {
+        //player = GetComponent<GameObject>();
+        player_camera = GetComponentInChildren<Camera>();
+        ani = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimationUpdate();
+
+    }
+
+    void FixedUpdate()
+    {
+        run();
+        turn();
+    }
+
+    void AnimationUpdate()
+    {
+        if(addPosition.x == 0 && addPosition.z == 0)
+        {
+            ani.SetBool("isRunning", false);
+        }
+        else
+        {
+            ani.SetBool("isRunning", true);
+        }
+    }
+
+    void run()
+    {
+        addPosition = Vector3.zero;
+        {
+            float s = 1.0f;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                //addPosition.z = 10;
+                s = 2.5f;
+            }
+            if (Input.GetKey(KeyCode.W) == true)
+            {
+                addPosition.z = speed * s;
+
+            }
+            if (Input.GetKey(KeyCode.S) == true)
+            {
+                addPosition.z = -speed * s;
+                player.transform.Rotate(0,180f,0,0);
+            }
+            if (Input.GetKey(KeyCode.A) == true)
+            {
+                addPosition.x = -speed * s;
+            }
+            if (Input.GetKey(KeyCode.D) == true)
+            {
+                addPosition.x = speed * s;
+            }
+        }
+        transform.position += ((transform.rotation * addPosition) * Time.deltaTime);
+
+    }
+
+    void turn()
+    {
+        float m_rotation_y = 0f;
+        m_rotation_y += (Input.GetAxis("Mouse X") * 10.0f);
+        transform.Rotate(0f, m_rotation_y, 0f, 0);
+
+
+        //Quaternion newRotation = Quaternion.LookRotation(addPosition);
+        //player.transform.rotation = Quaternion.Slerp(player.transform.rotation, newRotation, 5.0f * Time.deltaTime);
+
+
+        //player.transform.rotation = Quaternion.Euler(0f, m_rotation_y, 0f);
+
+        //transform.position = Vector3.zero;
+        //transform.rotation += ((player_camera.transform.rotation * addPosition) * Time.deltaTime);
+
+        //player_camera.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * 5.0f);
+    }
+}
