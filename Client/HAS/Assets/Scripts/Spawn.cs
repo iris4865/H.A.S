cf10bd89fc50c90e92ddc994527c9a53f3e8a615
mykim31 <dnwjd146@naver.com>
2017-04-20 13:46:52 +0900

    item spawn 작업 완료

---
+++ Client/HAS/Assets/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    int size = 10;
+    public GameObject trigger_object;
+    public GameObject[] spawn = new GameObject[10];    
+    Vector3 posi;
+    float range = 30.0f;
+
+    // Use this for initialization
+    void Start () {
+        int i;
+        
+        for (i = 0; i < size; i++)
+        {
+            posi = new Vector3(Random.Range(-range, range), 0f, Random.Range(-range, range));
+
+            Collider collider = spawn[i].GetComponent<BoxCollider>();
+            collider.isTrigger = true;
+            ((BoxCollider)collider).size = new Vector3(2f,2f,2f);
+
+            Instantiate(spawn[i], posi, transform.rotation);
+        }
+    }
+
+    void Awake(){
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
