80c23aa5f7784f6ad5fcc11fa7850ba3137d37ec
iris4865 <iris4865@gmail.com>
2017-04-22 01:18:25 +0900

    Remove annotation

--- HatchlingNet/Server/96.DataBase/MySqlAdapter.cs
+++ HatchlingNet/Server/96.DataBase/MySqlAdapter.cs
@@ -39,61 +39,14 @@ namespace DataBase
         {
             return MySqlHelper.ExecuteDataset(connection, sqlQuery);
         }
-
-        //오류나면 밑의 코드로 교체
-        //set nocount on이면 항상 -1로 반환하기때문에 문제발생가능
+        
         public bool SendQueryNoData(string sqlQuery)
         {
             if (MySqlHelper.ExecuteNonQuery(connection, sqlQuery) == -1)
                 return false;
 
             return true;
-            /*
-            MySqlCommand command = new MySqlCommand(sqlQuery, connection);
-
-            if (command.ExecuteNonQuery() == -1)
-                return false;
-
-            return true;
-            */
         }
-
-
-        //sendQuery 이전 코드
-        /*
-        public List<string> SendQuery2(string sqlQuery)
-        {
-            List<string> resultList = new List<string>();
-
-            MySqlCommand command = new MySqlCommand(sqlQuery, connection);
-            using (MySqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                    resultList.Add(GetLineString(reader));
-            }
-            return resultList;
-        }
-
-        private string GetLineString(MySqlDataReader reader)
-        {
-            string line = "";
-
-            for (int i = 0; i < reader.FieldCount; i++)
-            {
-                try
-                {
-                    line += reader.GetString(i);
-                }
-                catch (SqlNullValueException)
-                {
-                    line += "Null";
-                }
-                line += "\t";
-            }
-
-            return line;
-        }
-        */
     }
 
 }
